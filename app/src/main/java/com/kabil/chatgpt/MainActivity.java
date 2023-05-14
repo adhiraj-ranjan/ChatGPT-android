25e697826db1332f4299dd37d9226ad3eae81622
Adhiraj Ranjan <54994272+adhiraj-ranjan@users.noreply.github.com>
2023-05-14 18:33:40 +0530

    upload files

---
+++ app/src/main/java/com/kabil/chatgpt/MainActivity.java
@@ -0,0 +1,258 @@
+package com.kabil.chatgpt;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import java.util.Locale;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    ProgressBar progressBar;
+    ProgressDialog progressDialog;
+    private ActivityResultLauncher<Intent> someActivityResultLauncher;
+    Intent data;
+    TextToSpeech textToSpeech;
+
+    public String USER_AGENT = "(Android " + Build.VERSION.RELEASE + ") Chrome/110.0.5481.63 Mobile";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        progressBar = findViewById(R.id.progressBar);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Please Wait...");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setUserAgentString(USER_AGENT);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        webSettings.setDomStorageEnabled(true);
+
+
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.loadUrl("https://chat.openai.com");
+
+        /*Alertnate code for  startActivityForResult(intent,REQUEST_CODE_SPEECH_INPUT);
+          This code replaces deprecated startActivityForResult() method*/
+        someActivityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK) {
+                    // There are no request codes
+                    data = result.getData();
+                }
+            }
+        });
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textToSpeech.setLanguage(Locale.getDefault());
+                }
+            }
+        });
+        // Create an UtteranceProgressListener object to handle callbacks
+        UtteranceProgressListener utteranceProgressListener = new UtteranceProgressListener() {
+            @Override
+            public void onStart(String utteranceId) {
+                // Called when TTS starts speaking
+            }
+
+            @Override
+            public void onDone(String utteranceId) {
+                // Called when TTS finishes speaking
+            }
+
+            @Override
+            public void onError(String utteranceId) {
+                // Called when TTS encounters an error
+            }
+        };
+        // Add the UtteranceProgressListener to the TextToSpeech object
+        textToSpeech.setOnUtteranceProgressListener(utteranceProgressListener);
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                progressBar.setProgress(newProgress);
+                progressDialog.show();
+                if (newProgress == 100) {
+                    progressDialog.dismiss();
+                }
+
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+
+        } else {
+            closeApp();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.exit:
+                closeApp();
+                break;
+            case R.id.mic:
+                onMicPressed();
+                break;
+            case R.id.speak:
+                onSpeakerPressed();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onSpeakerPressed() {
+        String allParagraph;
+
+        try {
+
+            allParagraph = "var paragraphs = document.getElementsByClassName('markdown');" +
+                            "if (paragraphs.length){" +
+                                "paragraphs = paragraphs[paragraphs.length - 1].childNodes;" +
+                                "var combinedText='';" +
+                                "for (var i = 0; i < paragraphs.length; i++) {" +
+                                    "combinedText+=paragraphs[i].innerText;" +
+                                "}combinedText;" +
+                            "}";
+
+            webView.evaluateJavascript(allParagraph, new ValueCallback<String>() {
+                @Override
+                public void onReceiveValue(String value) {
+
+                    String utteranceId = UUID.randomUUID().toString();
+                    textToSpeech.speak(value, TextToSpeech.QUEUE_FLUSH, null, utteranceId);
+
+                }
+                });
+
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void onMicPressed() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "");
+
+        try {
+            someActivityResultLauncher.launch(intent);
+            webView.requestFocus();
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void closeApp() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Are you sure you want to Exit ?").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+            progressBar.setVisibility(View.VISIBLE);
+            webView.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(View.GONE);
+            webView.setVisibility(View.VISIBLE);
+            webView.requestFocus();
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (data!=null) {
+                String str = data.getStringArrayListExtra("android.speech.extra.RESULTS").get(0);
+                String stringBuilder = "(function() { var d = document.getElementsByTagName('textarea').length; document.getElementsByTagName('textarea')[d-1].value='" +
+                        str +
+                        "';document.querySelector('button.absolute').disabled = false;" +
+                        "document.querySelector('button.absolute').click(); })();";
+                webView.evaluateJavascript(stringBuilder, null);
+            }
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+}
